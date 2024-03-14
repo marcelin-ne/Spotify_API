7a87f54cd6794caa2217cd93a98c41930e91d94e
marcelinne <marcela.montalvo@epn.edu.ec>
2024-03-13 23:40:51 -0500

    test: Test fetching top tracks of a Spotify artist
    - Import spotipy library to interact with Spotify API
    - Use SpotifyClientCredentials for application authentication
    - Fetch top tracks of a specific artist
    - Print track names, audio preview URLs, and album cover art URLs

--- spotify-profile-demo/quickstar.py
+++ spotify-profile-demo/quickstar.py
@@ -1,20 +1,30 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-# Autenticación de la aplicación con Spotify
-auth_manager = SpotifyClientCredentials()
-spotify = spotipy.Spotify(auth_manager=auth_manager)
-
-# URI del artista
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-# Recuperar los álbumes del artista
+
 results = spotify.artist_albums(birdy_uri, album_type='album')
 albums = results['items']
 while results['next']:
     results = spotify.next(results)
     albums.extend(results['items'])
 
-# Imprimir el nombre de cada álbum
 for album in albums:
     print(album['name'])
+    
+# Get the top tracks for an artist
+
+print("Led Zeppelin")
+lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+results = spotify.artist_top_tracks(lz_uri)
+
+#30 second samples and cover art for the top 10 tracks for Led Zeppelin:
+
+
+for track in results['tracks'][:10]:
+    print('track    : ' + track['name'])
+    print('audio    : ' + track['preview_url'])
+    print('cover art: ' + track['album']['images'][0]['url'])
+    print()
