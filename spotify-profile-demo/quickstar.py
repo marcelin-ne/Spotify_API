c94d3b41b254eb82bb35665a5f282f7553d1f76e
marcelinne <marcela.montalvo@epn.edu.ec>
2024-03-13 23:59:48 -0500

    feat: Add code to retrieve user's top artists with different time ranges
    - Define scope as 'user-top-read' to access user's top artists data
    - Initialize spotipy.Spotify with SpotifyOAuth authentication and defined scope
    - Print user's top 10 artists for different time ranges: short_term, medium_term, long_term

--- spotify-profile-demo/quickstar.py
+++ spotify-profile-demo/quickstar.py
@@ -39,3 +39,20 @@ print("My Playlists:")
 results = sp.current_user_playlists(limit=50)
 for i, item in enumerate(results['items']):
     print("%d %s" % (i, item['name']))
+    
+#my top artist 
+scope = 'user-top-read'
+ranges = ['short_term', 'medium_term', 'long_term']
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+print("My top 10 artists:")
+
+for sp_range in ['short_term', 'medium_term', 'long_term']:
+    print("range:", sp_range)
+
+    results = sp.current_user_top_artists(time_range=sp_range, limit=50)
+
+    for i, item in enumerate(results['items']):
+        print(i, item['name'])
+    print()
